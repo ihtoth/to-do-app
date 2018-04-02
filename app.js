5838ef7e24a162cc7a084434975f2a6eb81e9047
Ildikó Tóth <ildiko@ildikototh.com>
2018-04-02 13:07:54 -0700

    Checkpoint 6 DOM scripting events part 2 completed (without bonus)

--- app.js
+++ app.js
@@ -1,95 +1,86 @@
-function removeLi(id){
-  const toDoList = document.getElementById('toDoList');
-  toDoList.removeChild(document.getElementById(id));
-}
 
 function onReady(){
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const removeLiFromList = document.getElementById('removeLi');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  let id = 0;
 
-  let liCounter = 0;                                        //counts up from 0, supplies a unique id to each new element added
-  let numLi = 0;                                            //keeps track of the number of list items.
-
-  addToDoForm.addEventListener('submit', ()=>{
-    event.preventDefault();                                  //prevents page from reloading
-
-    let title = newToDoText.value;                           //saves the new to do item submitted
-
-    let newLi = document.createElement('li');                //creates a new list element
-    newLi.className = "mdl-list__item";
-    newLi.setAttribute("id", `list-item-${liCounter}`);      //adds material lite class to li element
+  function createNewToDo(){
+    const newToDoText = document.getElementById('newToDoText');
+    //disallow adding empty list item to list
+    if(!newToDoText.value){return;}
 
 
-    let newSpan = document.createElement('span');            //create span to house li
-    newSpan.className = "mdl-list__item-primary-content";    //adds material lite class to span element
-    newSpan.textContent = title;                             //sets the Span element to the to do item
+    toDos.push({title:newToDoText.value, complete:false, id:id++});            //literal notation add to array.
 
-    //for the checkbox
-    let checkboxSpan = document.createElement('span');
-    checkboxSpan.className="mdl-list__item-secondary-action mdl-checkbox__label";
+    newToDoText.value = '';
 
-    let checkboxLabel = document.createElement('label');
-    checkboxLabel.className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
-     checkboxLabel.setAttribute("for", `list-checkbox-${liCounter}`); //adds unique for to label element
+    renderTheUI();
+  }
 
-    let checkboxInput = document.createElement('input');              //creates input...
-    checkboxInput.type = "checkbox";                                  //makes the input checkbox an actual checkbox
-    checkboxInput.className="mdl-checkbox__input";
-    checkboxInput.setAttribute("id", `list-checkbox-${liCounter}`);   //adds unique id to label element
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
 
-    //add checkbox to label, label to span.
-    checkboxLabel.appendChild(checkboxInput);
-    checkboxSpan.appendChild(checkboxLabel);
+    //reset the newLi's text content
+    toDoList.textContent = '';
 
-    //adds checkbox span (the container for all checkbox related elements) to the new list item.
-    newLi.appendChild(checkboxSpan);
-    newLi.appendChild(newSpan);                               //adds to do item title to list
+    toDos.forEach(function(toDo){
+        //create the UI we need for each to-do(the Li and checkbox)
+        const newItemText = document.createElement('span');            //create span to house li
+        newItemText.className = "liText mdl-list__item-primary-content";
+
+        const newLi = document.createElement('li');
+        newLi.className = "mdl-list__item";
+
+        //for the checkbox
+        const checkboxSpan = document.createElement('span');
+        checkboxSpan.className="mdl-list__item-secondary-action mdl-checkbox__label";
+
+        const checkboxLabel = document.createElement('label');
+        checkboxLabel.className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
+        checkboxLabel.setAttribute("for", `list-checkbox-${toDo.id}`);
+
+        const checkbox = document.createElement('input');
+        checkbox.className="mdl-checkbox__input";
+        checkbox.setAttribute("id", `list-checkbox-${toDo.id}`);
+        checkbox.type = 'checkbox';
+
+        //for the delete button attached to each list item
+        const deleteButton = document.createElement('button');
+        deleteButton.className = "mdl-button mdl-js-button mdl-button--accent";
+        deleteButton.textContent = "delete";
+        deleteButton.type = 'button';
+        deleteButton.addEventListener('click', event =>{
+          toDos = toDos.filter(item => item.id != toDo.id);
+          renderTheUI();
+        });
+
+        //Assign the toDo's title to the list item
+        newItemText.textContent = toDo.title;
+
+        /*** update the DOM with the li and checkbox ***/
+
+        //add checkbox to label, label to span.
+        checkboxLabel.appendChild(checkbox);
+        checkboxSpan.appendChild(checkboxLabel);
+
+        newLi.appendChild(checkboxSpan);
+        newLi.appendChild(newItemText);
+        newLi.appendChild(deleteButton);
+        toDoList.appendChild(newLi);
 
 
-    toDoList.appendChild(newLi);                              //adds the new to do item list element to the unordered list (ul)
+    });
 
-    liCounter++;                                              //liCounter +1 for next item added.
+  }
 
-    if(++numLi == 1){                                         //Add one for when adding first Li, subtract when removing one.
-      removeLiFromList.classList.remove("mdl-button--disabled");
-      removeLiFromList.classList.add("mdl-button--color");
-    }
-
-    newToDoText.value = '';                                  //set the text in the list item input field to blank.
+  addToDoForm.addEventListener('submit', event =>{
+    event.preventDefault();
+    createNewToDo();
 
   });
 
+  renderTheUI();
 
-  //does allCheckboxes update every time there's a removal of an li?
-  const allCheckboxes = document.getElementsByClassName("mdl-checkbox__input");
-
-  removeLiFromList.addEventListener('click',() => {
-
-    //let checkedCheckboxes = allCheckboxes.filter(c => c > 1); //Why does this not work?
-
-    let liNumList = [];
-    //find which checkboxes are checked.
-    for (let i = 0; i<allCheckboxes.length; i++){
-      let currentID = allCheckboxes[i].id;
-
-      if(allCheckboxes[i].checked){
-        console.log(currentID.slice(currentID.lastIndexOf('-')+1));
-        liNumList.push(currentID.slice(currentID.lastIndexOf('-')+1));
-        }
-    }
-    //remove checked list elements
-    for(let i = 0; i<liNumList.length; i++){
-      removeLi(`list-item-${liNumList[i]}`);
-      numLi--;
-    }
-    //change remove button's appearance back to disabled.
-    if (numLi == 0){
-      removeLiFromList.classList.remove("mdl-button--color");
-      removeLiFromList.classList.add("mdl-button--disabled");
-    }
-  });
 }
 
 window.onload = function(){
