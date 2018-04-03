a9859b495488c15688df127a7d71b776939eded8
Ildikó Tóth <ildiko@ildikototh.com>
2018-04-03 14:56:56 -0700

    indented app.js and index.html for better reading

--- app.js
+++ app.js
@@ -24,51 +24,51 @@ function onReady(){
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo){
-        //create the UI we need for each to-do(the Li and checkbox)
-        const newItemText = document.createElement('span');            //create span to house li
-        newItemText.className = "liText mdl-list__item-primary-content";
+      //create the UI we need for each to-do(the Li and checkbox)
+      const newItemText = document.createElement('span');            //create span to house li
+      newItemText.className = "liText mdl-list__item-primary-content";
 
-        const newLi = document.createElement('li');
-        newLi.className = "mdl-list__item";
+      const newLi = document.createElement('li');
+      newLi.className = "mdl-list__item";
 
-        //for the checkbox
-        const checkboxSpan = document.createElement('span');
-        checkboxSpan.className="mdl-list__item-secondary-action mdl-checkbox__label";
+      //for the checkbox
+      const checkboxSpan = document.createElement('span');
+      checkboxSpan.className="mdl-list__item-secondary-action mdl-checkbox__label";
 
-        const checkboxLabel = document.createElement('label');
-        checkboxLabel.className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
-        checkboxLabel.setAttribute("for", `list-checkbox-${toDo.id}`);
+      const checkboxLabel = document.createElement('label');
+      checkboxLabel.className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect";
+      checkboxLabel.setAttribute("for", `list-checkbox-${toDo.id}`);
 
-        const checkbox = document.createElement('input');
-        checkbox.className="mdl-checkbox__input";
-        checkbox.setAttribute("id", `list-checkbox-${toDo.id}`);
-        checkbox.type = 'checkbox';
+      const checkbox = document.createElement('input');
+      checkbox.className="mdl-checkbox__input";
+      checkbox.setAttribute("id", `list-checkbox-${toDo.id}`);
+      checkbox.type = 'checkbox';
 
-        //for the delete button attached to each list item
-        const deleteButton = document.createElement('button');
-        deleteButton.className = "mdl-button mdl-js-button mdl-button--accent";
-        deleteButton.textContent = "delete";
-        deleteButton.type = 'button';
-        deleteButton.addEventListener('click', event =>{
-          toDos = toDos.filter(item => item.id != toDo.id);
-          renderTheUI();
-        });
+      //for the delete button attached to each list item
+      const deleteButton = document.createElement('button');
+      deleteButton.className = "mdl-button mdl-js-button mdl-button--accent";
+      deleteButton.textContent = "delete";
+      deleteButton.type = 'button';
+      deleteButton.addEventListener('click', event =>{
+        toDos = toDos.filter(item => item.id != toDo.id);
+        renderTheUI();
+      });
 
-        //Assign the toDo's title to the list item
-        newItemText.textContent = toDo.title;
+      //Assign the toDo's title to the list item
+      newItemText.textContent = toDo.title;
 
-        /*** update the DOM with the li and checkbox ***/
+      /*** update the DOM with the li and checkbox ***/
 
-        //add checkbox to label, label to span.
-        checkboxLabel.appendChild(checkbox);
-        checkboxSpan.appendChild(checkboxLabel);
+      //add checkbox to label, label to span.
+      checkboxLabel.appendChild(checkbox);
+      checkboxSpan.appendChild(checkboxLabel);
 
-        newLi.appendChild(checkboxSpan);
-        newLi.appendChild(newItemText);
-        newLi.appendChild(deleteButton);
-        toDoList.appendChild(newLi);
+      newLi.appendChild(checkboxSpan);
+      newLi.appendChild(newItemText);
+      newLi.appendChild(deleteButton);
+      toDoList.appendChild(newLi);
 
-        componentHandler.upgradeDom();
+      componentHandler.upgradeDom();
     });
 
   }
